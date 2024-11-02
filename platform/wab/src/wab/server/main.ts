45f69ed885293a211d078634d472e388d16c8d1c
willymwai <willymwaid92@gmail.com>
2024-11-02 08:23:16 +0300

    fix(wab): ensure Elastic APM starts before app initialization

--- platform/wab/src/wab/server/main.ts
+++ platform/wab/src/wab/server/main.ts
@@ -1,17 +1,17 @@
 // tslint:disable:ordered-imports
 import { spawn } from "@/wab/shared/common";
 import { appBackendMain } from "@/wab/server/app-backend-real";
+if (process.env.ENABLE_ELASTIC_APM) {
+  require("elastic-apm-node").start({
+    serviceName: process.env.ELASTIC_APM_SERVICE_NAME ?? "wab",
+
+    secretToken: process.env.ELASTIC_APM_SECRET_TOKEN,
+
+    serverUrl: process.env.ELASTIC_APM_SERVER_URL,
+
+    environment: process.env.NODE_ENV,
+  });
+}
 if (require.main === module) {
-  if (process.env.ENABLE_ELASTIC_APM) {
-    require('elastic-apm-node').start({
-      serviceName: process.env.ELASTIC_APM_SERVICE_NAME ?? 'wab',
-
-      secretToken: process.env.ELASTIC_APM_SECRET_TOKEN,
-
-      serverUrl: process.env.ELASTIC_APM_SERVER_URL,
-
-      environment: process.env.NODE_ENV,
-    })
-  }
   spawn(appBackendMain());
 }
