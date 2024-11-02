fe3822511969884c09c31624828047847b90f07d
willymwai <willymwaid92@gmail.com>
2024-11-02 07:34:05 +0300

    feat(wab): integrate Elastic APM for performance monitoring

--- platform/wab/src/wab/server/main.ts
+++ platform/wab/src/wab/server/main.ts
@@ -2,5 +2,16 @@
 import { spawn } from "@/wab/shared/common";
 import { appBackendMain } from "@/wab/server/app-backend-real";
 if (require.main === module) {
+  if (process.env.ENABLE_ELASTIC_APM) {
+    require('elastic-apm-node').start({
+      serviceName: process.env.ELASTIC_APM_SERVICE_NAME ?? 'wab',
+
+      secretToken: process.env.ELASTIC_APM_SECRET_TOKEN,
+
+      serverUrl: process.env.ELASTIC_APM_SERVER_URL,
+
+      environment: process.env.NODE_ENV,
+    })
+  }
   spawn(appBackendMain());
 }
