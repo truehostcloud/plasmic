0457c99821b563ed1e2f9c231b19ea6dc4d4a7e3
willymwai <willymwaid92@gmail.com>
2024-11-04 14:08:31 +0300

    fix(devflags): use environment variable for admin team domain in Docker setup

--- platform/wab/src/wab/shared/devflags.ts
+++ platform/wab/src/wab/shared/devflags.ts
@@ -394,7 +394,7 @@ const DEFAULT_DEVFLAGS = {
     process.env.CODEGEN_ORIGIN_HOST ||
     process.env.CODEGEN_HOST ||
     "http://codegen-origin.plasmic.app",
-  adminTeamDomain: production ? process.env.ADMIN_TEAM_DOMAIN : "admin.example.com",
+  adminTeamDomain: process.env.ADMIN_TEAM_DOMAIN,
   defaultHostUrl:
     process.env.REACT_APP_DEFAULT_HOST_URL ||
     "https://host.plasmicdev.com/static/host.html",
