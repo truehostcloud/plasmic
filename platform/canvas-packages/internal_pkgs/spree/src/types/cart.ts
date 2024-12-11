6c1556e9a9f5d29f9f7e16ba4bd115bee5741cf3
willymwai <willymwaid92@gmail.com>
2024-12-11 11:14:07 +0300

    refactor: update product variant type imports and add measurement interfaces

--- platform/canvas-packages/internal_pkgs/spree/src/types/cart.ts
+++ platform/canvas-packages/internal_pkgs/spree/src/types/cart.ts
@@ -2,7 +2,7 @@
   Forked from https://github.com/vercel/commerce/tree/main/packages/saleor/src
   Changes: None
 */
-
+import type { Image, Measurement } from './common'
 import { CartType as Core } from '@plasmicpkgs/commerce'
 
 
@@ -36,6 +36,63 @@ export type GetCartHandler = CartHandlers['getCart']
 export type AddItemHandler = CartHandlers['addItem']
 export type UpdateItemHandler = CartHandlers['updateItem']
 export type RemoveItemHandler = CartHandlers['removeItem']
+
+export interface ProductVariant {
+  /**
+   *  The unique identifier for the variant.
+   */
+  id: string
+  /**
+   * The SKU (stock keeping unit) associated with the product variant.
+   */
+  sku?: string
+  /**
+   * The product variant’s name, or the product's name.
+   */
+  name: string
+  /**
+   * The product variant’s price after all discounts are applied.
+   */
+  price: number
+  /**
+   * The product variant’s price before discounts are applied.
+   */
+  listPrice: number
+  /**
+   * Indicates if the variant is available for sale.
+   */
+  availableForSale?: boolean
+  /**
+   * Whether a customer needs to provide a shipping address when placing
+   * an order for the product variant.
+   */
+  requiresShipping?: boolean
+  /**
+   * The image associated with the variant.
+   */
+  image?: Image
+  /**
+   * The variant's weight. If a weight was not explicitly specified on the
+   * variant, this will be the product's weight.
+   */
+  weight?: Measurement
+  /**
+   * The variant's height. If a height was not explicitly specified on the
+   * variant, this will be the product's height.
+   */
+  height?: Measurement
+  /**
+   * The variant's width. If a width was not explicitly specified on the
+   * variant, this will be the product's width.
+   */
+  width?: Measurement
+  /**
+   * The variant's depth. If a depth was not explicitly specified on the
+   * variant, this will be the product's depth.
+   */
+  depth?: Measurement
+}
+
 
 export interface SelectedOption {
   /**
--- platform/canvas-packages/internal_pkgs/spree/src/types/common.ts
+++ platform/canvas-packages/internal_pkgs/spree/src/types/common.ts
@@ -1,3 +1,21 @@
+export interface Discount {
+  /**
+   * The value of the discount, can be an amount or percentage.
+   */
+  value: number
+}
+
+export interface Measurement {
+  /**
+   * The measurement's value.
+   */
+  value: number
+  /**
+   * The measurement's unit, such as "KILOGRAMS", "GRAMS", "POUNDS" & "OOUNCES".
+   */
+  unit: 'KILOGRAMS' | 'GRAMS' | 'POUNDS' | 'OUNCES'
+}
+
 export interface Image {
   /**
    * The URL of the image.
