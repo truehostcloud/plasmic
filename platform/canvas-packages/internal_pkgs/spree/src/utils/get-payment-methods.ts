9ae7c99c6a95073db6710fe3de9dcde773040b9d
willymwai <willymwaid92@gmail.com>
2025-01-27 16:09:03 +0300

    feat(spree): make payment method fields optional and normalize payment methods response

--- platform/canvas-packages/internal_pkgs/spree/src/utils/get-payment-methods.ts
+++ platform/canvas-packages/internal_pkgs/spree/src/utils/get-payment-methods.ts
@@ -3,24 +3,34 @@ import { HookFetcherContext } from '@plasmicpkgs/commerce'
 import type { IToken } from '@spree/storefront-api-v2-sdk/types/interfaces/Token'
 import ensureIToken from './tokens/ensure-itoken'
 import { IPaymentMethods } from '@spree/storefront-api-v2-sdk/types/interfaces/PaymentMethod'
+import { PaymentMethod } from '../commerce/types/checkout'
 
 const getPaymentMethods = async (
   fetch: HookFetcherContext<{
     data: any
   }>['fetch']
-): Promise<IPaymentMethods> => {
+): Promise<PaymentMethod[]> => {
   const token: IToken | undefined = ensureIToken()
 
   if (!token) {
     return null
   } else {
-    const { data } = await fetch<GraphQLFetcherResult<IPaymentMethods>>({
+    const { data: spreeSuccessResponse } = await fetch<
+      GraphQLFetcherResult<IPaymentMethods>
+    >({
       variables: {
         methodPath: 'checkout.paymentMethods',
         arguments: [token, {}],
       },
     })
-    return data
+    return spreeSuccessResponse.data.map((paymentMethod): PaymentMethod => {
+      return {
+        id: paymentMethod.id,
+        type: paymentMethod.attributes.type,
+        name: paymentMethod.attributes.name,
+        description: paymentMethod.attributes.description,
+      }
+    })
   }
 }
 
